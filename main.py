b7588daaaf956b65863888df6423de64cff02df8
Artem Suttar <artem.suttar@gmail.com>
2025-02-21 10:12:25 +0100

    change colour to dot

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def use_tflite_model(name, img_path):
     if binary_y_pred == 1:
         color = (0, 255, 0)
     else:
-        color = (255, 0, 0)
+        color = (0, 0, 255)
 
     resize_factor = 5
 
